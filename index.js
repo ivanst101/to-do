5575fc31a5797e96642953e4c8da8106b5b6d525
ivanst101 <98849440+ivanst101@users.noreply.github.com>
2024-02-19 22:51:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const buttonAdd = document.getElementById("btn");
+
+ 
+buttonAdd.addEventListener("click", function(){
+
+    if(inputBox.value === ""){
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value = "";
+    saveData();
+
+});
+
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+    console.log(listContainer.textContent);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
